005ca2ebace30405028611d2ddc26ea16e4f3585
anoopmathews <73633044+anoopmathews@users.noreply.github.com>
2020-10-31 02:51:49 +0530

    Update App.java

--- src/main/java/com/sample/maven/mvntest/App.java
+++ src/main/java/com/sample/maven/mvntest/App.java
@@ -11,24 +11,11 @@ import org.testng.TestNG;
  */
 public class App {
 
-    /*public static void main(String[] args) {
-
-        if (args.length < 1) {
-            System.err.println("Please provide an input!");
-            System.exit(0);
-        }
-        System.out.println(sha256hex(args[0]));
-
-    }
-
-    public static String sha256hex(String input) {
-        return DigestUtils.sha256Hex(input);
-    }*/
-	
+    
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
-		TestNG testng = new TestNG();
+	TestNG testng = new TestNG();
         ArrayList<String> suites = new ArrayList<String>();
         suites.add(System.getProperty("user.dir")+"\\config\\testng.xml");
         testng.setTestSuites(suites);
